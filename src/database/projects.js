4f6884795f01effee7ebc05b667ccf3f50ed1fe1
dawidbros1 <dawidbros1@wp.pl>
2022-11-08 09:27:44 +0100

    update

--- src/database/projects.js
+++ src/database/projects.js
@@ -6,21 +6,21 @@ class ProjectDatabase {
 
    init() {
       this.projects.push({
-         name: "Youtube subscription manager",
+         name: "Aplikacja do segregowania subskrypcji z YouTube [ PHP ]",
          description: `Jest to aplikacja, która umożliwia segregowanie subskrypcji z serwisu YouTube poprzez dodanie ich do stworzonych grup.`,
          git: "https://github.com/dawidbros1/youtube-subscriptions-manager",
       })
 
       this.projects.push({
          name: "Podstawka do tworzenia aplikacji w technologii PHP",
-         description: `Stworzona podstawka to gotowy pakiet startowy plików do tworzenia aplikacji w technologii PHP. 
+         description: `Jest to gotowy pakiet startowy plików do tworzenia aplikacji w technologii PHP. 
              Starter posiada podstawowe funkcjonalności takie jak rejestracja/logowanie, edycję profilu oraz możliwość przywrócenia hasła. 
              Backend aplikacji posiada podział na modele, repozytoria, kontrolery, szablony widoków itp.`,
          git: "https://github.com/dawidbros1/php-start",
       })
 
       this.projects.push({
-         name: "System do zarządzania zakładkami [ PHP ]",
+         name: "Aplikacja do zarządzania zakładkami [ PHP ]",
          description: `Jest to system, który umożliwia udostępnienie wiele linków za pomocą jednego linka. 
              System oferuje możliwość tworzenia pojemników (tzw. kategorii), do których następnie możemy dodawać linki do stron.
              Strony można przenosić między różnymi pojemnikami. Każda kategoria jest opatrzona statusem prywatności, co zapewnia bezpieczeństwo naszych prywatnych danych.
@@ -30,14 +30,14 @@ class ProjectDatabase {
       })
 
       this.projects.push({
-         name: "System do zarządzania zakładkami [ LARAVEL ]",
+         name: "Aplikacja do zarządzania zakładkami [ LARAVEL ]",
          description: `Wersja projektu, która została napisana w LARAVEL, ma poszerzoną strukturę bazy danych o podfoldery. 
-            Każdy pojedynczy element został zaopatrzony w status prywatności oraz widoczności. Dodatkowo system umożliwia sortowanie elementów wedle własnego uznania.`,
+            Każdy pojedynczy element został zaopatrzony w status prywatności oraz widoczności. Dodatkowo aplikacja umożliwia sortowanie elementów wedle własnego uznania.`,
          git: "https://github.com/dawidbros1/bookmarks-laravel",
       })
 
       this.projects.push({
-         name: "Forum [ Symfony ]",
+         name: "Proste forum internetowe [ Symfony ]",
          description: `Jest to proste forum umożliwiające tworzenie wątków dla administratora, a także tematów, postów i komentarzy dla użytkowników.`,
          git: "https://github.com/dawidbros1/forum",
       })
